a5d5ff111c307940ea6106a72f42a45773cfe6ee
chaeyoungwon <epopcy@naver.com>
2024-11-09 01:28:43 +0900

    Chore: 프로젝트 폴더 구조 세팅 및 웹 앱 레이아웃 구현

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './Home.Styled';
+
+const Home: React.FC = () => {
+  return <S.Container>Hi</S.Container>;
+};
+export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,5 @@
+const theme = {
+  colors: {},
+  fontStyles: {},
+};
+export default theme;
