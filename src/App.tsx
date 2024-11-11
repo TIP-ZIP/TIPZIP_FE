9682dc7cd1179d7988ffc1fb2a600668c4d25a63
DefineXX <keaikim21@naver.com>
2024-11-11 19:05:17 +0900

    refactor(layout): layout 디렉토리 src/layout로 이동 (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from '@pages/home/Home';
-import Layout from '@components/layout/Layout';
+import Layout from './layout/Layout';
 
 function App() {
   return (
