e6be9e0544836e60a651575ddfd9088ed56573b6
chaeyoungwon <epopcy@naver.com>
2024-11-18 21:07:04 +0900

    feat(mypage): 공통 컴포넌트 생성 및 로그아웃 모달 구현

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './Header.Styled';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { LogoutModal } from '@components/modal/LogoutModal';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
 
   // 추후 로그인 로직 추가
   const isLoggedIn = true;
 
+  const handleLogoutClick = () => {
+    setShowLogoutModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowLogoutModal(false);
+  };
+  const handleLogout = () => {
+    // 로그아웃 로직을 여기에 추가
+    setShowLogoutModal(false);
+  };
+
   return (
-    <S.Container>
-      <S.ItemList>
-        <S.Logo onClick={() => navigate('/home')} />
-        <S.IconList>
-          {isLoggedIn && location.pathname === '/mypage' && (
+    <>
+      <S.Container>
+        <S.ItemList>
+          <S.Logo onClick={() => navigate('/home')} />
+          <S.IconList>
+            {isLoggedIn && location.pathname === '/mypage' && (
+              <S.Icon onClick={handleLogoutClick}>
+                <S.LogoutIcon />
+              </S.Icon>
+            )}
             <S.Icon>
-              <S.LogoutIcon />
+              <S.BookmarkIcon />
             </S.Icon>
-          )}
-          <S.Icon>
-            <S.BookmarkIcon />
-          </S.Icon>
-          <S.Icon>
-            <S.ProfileIcon onClick={() => navigate('/mypage')} />
-          </S.Icon>
-        </S.IconList>
-      </S.ItemList>
-    </S.Container>
+            <S.Icon>
+              <S.ProfileIcon onClick={() => navigate('/mypage')} />
+            </S.Icon>
+          </S.IconList>
+        </S.ItemList>
+      </S.Container>
+      <LogoutModal showModal={showLogoutModal} onClose={handleCloseModal} onLogout={handleLogout} />
+    </>
   );
 };
 export default Header;
--- src/components/modal/LogoutModal.tsx
+++ src/components/modal/LogoutModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as S from './LoginModal.Styled';
+import { Modal } from './Modal';
 
 export const LogoutModal: React.FC<{
   showModal: boolean;
@@ -7,17 +7,16 @@ export const LogoutModal: React.FC<{
   onLogout: () => void;
 }> = ({ showModal, onClose, onLogout }) => {
   if (!showModal) return null;
-
   return (
-    <S.ModalOverlay onClick={onClose}>
-      <S.ModalContainer onClick={(e) => e.stopPropagation()}>
-        <S.CloseButton onClick={onClose} />
-        <S.ModalTitle>정말 로그아웃하시겠습니까?</S.ModalTitle>
-        <S.ButtonContainer>
-          <S.ModalButton onClick={onClose}>취소</S.ModalButton>
-          <S.ModalButton1 onClick={onLogout}>네</S.ModalButton1>
-        </S.ButtonContainer>
-      </S.ModalContainer>
-    </S.ModalOverlay>
+    <Modal
+      showModal={showModal}
+      onClose={onClose}
+      onAction={onLogout}
+      title='정말 로그아웃하시겠습니까?'
+      buttonText1='취소'
+      buttonText2='네'
+      buttonPadding1='1.2rem 5.1rem'
+      buttonPadding2='1.2rem 5.1rem'
+    />
   );
 };
