69ce369d97558753616b902302fa72a628ba5aa2
DefineXX <keaikim21@naver.com>
2024-11-23 12:37:04 +0900

    fix(setUsername): 사용자 이름 입력 시 IME 조합 상태 추가 반영 (for 한글) (#6)

--- src/pages/SetUsername/SetUsername.tsx
+++ src/pages/SetUsername/SetUsername.tsx
@@ -1,16 +1,44 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import * as S from './SetUsername.styled';
 
 const SetUsername: React.FC = () => {
   const MAX_NICKNAME_LENGTH = 12;
 
-  const [nickname, setNickname] = useState('');
-  const [invalidMessage, setInvalidMessage] = useState('');
-  const [submitAvailable, setSubmitAvailable] = useState(false);
+  const nicknameInputRef = useRef<HTMLInputElement | null>(null);
+  const saveButtonRef = useRef<HTMLButtonElement | null>(null);
+
+  const [nickname, setNickname] = useState<string>('');
+  const [isComposing, setIsComposing] = useState<boolean>(false);
+  const [invalidMessage, setInvalidMessage] = useState<string>('');
+  const [submitAvailable, setSubmitAvailable] = useState<boolean>(false);
+
+  const handleCompositionStart = () => {
+    setIsComposing(true);
+    // console.log('IME 조합 시작');
+  };
+
+  const handleCompositionEnd = (e: React.CompositionEvent<HTMLInputElement>) => {
+    setIsComposing(false);
+    // console.log('IME 조합 완료', e.currentTarget.value);
+    setNickname(e.currentTarget.value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!isComposing && e.key === 'Enter') {
+      e.preventDefault(); // Enter 키가 폼 제출 동작으로 해석됨으로 인해 발생하는 중복 동작 방지
+      saveButtonRef.current?.click();
+    }
+  };
+
+  const handleSaveButtonClick = () => {
+    // 추후 API 연동 시 사용자 이름 POST 로직 필요
+    alert('사용자 이름 설정 완료!');
+    nicknameInputRef.current?.blur(); // 저장 버튼 클릭 시 Input 포커스 해제
+  };
 
   const handleNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const nicknameInput = e.target.value;
+    const nicknameInput = e.target.value.replace(/\s+/g, ''); // 모든 공백 문자(Enter, SpaceBar, Tab) 제거
     console.log(nicknameInput);
 
     const nicknameRegex = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9]*$/;
@@ -41,6 +69,10 @@ const SetUsername: React.FC = () => {
           <S.NicknameInput
             onChange={handleNickname}
             value={nickname}
+            onCompositionStart={handleCompositionStart}
+            onCompositionEnd={handleCompositionEnd}
+            onKeyDown={handleKeyPress}
+            ref={nicknameInputRef}
             placeholder='사용할 이름을 입력하세요'
             maxLength={MAX_NICKNAME_LENGTH}
           />
@@ -53,7 +85,13 @@ const SetUsername: React.FC = () => {
           • 특수문자는 사용할 수 없어요 <br /> • 12자 이내로 설정해주세요
         </S.Restriction>
       </S.InputSection>
-      <S.SubmitButton disabled={!submitAvailable}>저장</S.SubmitButton>
+      <S.SubmitButton
+        ref={saveButtonRef}
+        onClick={handleSaveButtonClick}
+        disabled={!submitAvailable}
+      >
+        저장
+      </S.SubmitButton>
     </S.Container>
   );
 };
