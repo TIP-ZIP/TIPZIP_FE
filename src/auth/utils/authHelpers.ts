68ea40cb74c954a3f5e4ba84b0c0d9da110003fa
DefineXX <keaikim21@naver.com>
2024-12-24 02:10:07 +0900

    refactor(kakao): postAuthCodeToServer 함수 타입 추가 정의 (#16)

--- src/auth/utils/authHelpers.ts
+++ src/auth/utils/authHelpers.ts
@@ -1,6 +1,22 @@
 import axiosInstance from '@api/axios';
 
-export const postAuthCodeToServer = async (socialProvider: string, code: string) => {
+type SocialProvider = 'KAKAO' | 'GOOGLE' | 'NAVER';
+
+type AuthResponse = {
+  status: number;
+  data: {
+    access_token: string;
+    badge: boolean;
+    message: string;
+    user_id: string;
+    username: null | string;
+  };
+};
+
+export const postAuthCodeToServer = async (
+  socialProvider: SocialProvider,
+  code: string,
+): Promise<AuthResponse | undefined> => {
   try {
     const response = await axiosInstance.post(
       '/auth/login',
