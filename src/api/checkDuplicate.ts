651660e2b494d070b912f15b04dc3ab860c710f7
Chul Heung Kim <153730243+DefineXX@users.noreply.github.com>
2024-12-27 02:33:59 +0900

    Revert "Revert "Feat: 카카오 로그인 API 구현 (#26)""

--- src/api/checkDuplicate.ts
+++ src/api/checkDuplicate.ts
@@ -1,17 +1,36 @@
-import axios from 'axios';
+import axiosInstance from '@api/axios';
 
-const checkDuplicate = async (username: string): Promise<string | undefined> => {
+const checkDuplicate = async (username: string): Promise<void> => {
   try {
-    const response = await axios.post('auth/username-valid', {
-      username: username,
-    });
+    const response = await axiosInstance.post(
+      '/auth/username',
+      {
+        username: username,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
 
     // 추후 API 연동 시 Axios Instance 변경 및 Response 형식 반영
-    if (response) alert('중복 검사 완료!');
-
-    return response.data.message;
+    switch (response.status) {
+      case 200:
+        alert('중복 검사 완료!');
+        break;
+      case 400:
+        alert('사용중인 닉네임입니다.');
+        break;
+      case 500:
+        console.warn('Internal Server Error');
+        break;
+      default:
+        console.warn('Unknow Error');
+    }
   } catch (error) {
     console.error('사용자명 중복 확인 오류: ', error);
+    throw new Error('사용자명 중복 확인 오류가 발생했습니다.');
   }
 };
 
