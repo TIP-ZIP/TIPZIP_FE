1d5bdffa493a1df5190f5515b5bd58ae6c5e95f9
DefineXX <keaikim21@naver.com>
2024-12-28 18:33:04 +0900

    feat(Naver):  Naver 로그인 API 구현 (Kakao, Google과 동일한 로직) (#34)

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -14,10 +14,17 @@ const Login: React.FC = () => {
   const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   const GOOGLE_REDIRECT_URI = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
 
+  const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
+  const NAVER_REDIRECT_URI = import.meta.env.VITE_NAVER_REDIRECT_URI;
+  const NAVER_STATE_STRING = 'test';
+
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}`;
 
   const GoogleAuthBaseURL = 'https://accounts.google.com/o/oauth2/v2/auth';
   const GOOGLE_AUTH_URL = `${GoogleAuthBaseURL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20profile%20email`;
+
+  const NaverAuthBaseURL = 'https://nid.naver.com/oauth2.0/authorize';
+  const NAVER_AUTH_URL = `${NaverAuthBaseURL}?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_STATE_STRING}&redirect_uri=${NAVER_REDIRECT_URI}`;
 
   const handleKakaoLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
@@ -25,6 +32,10 @@ const Login: React.FC = () => {
 
   const handleGoogleLogin = () => {
     window.location.href = GOOGLE_AUTH_URL;
+  };
+
+  const handleNaverLogin = () => {
+    window.location.href = NAVER_AUTH_URL;
   };
 
   return (
@@ -38,7 +49,7 @@ const Login: React.FC = () => {
           카카오로 로그인
           <S.SocialIcon src={KakaoIcon} />
         </S.KakaoButton>
-        <S.NaverButton>
+        <S.NaverButton onClick={handleNaverLogin}>
           네이버로 로그인
           <S.NaverIcon src={NaverIcon} />
         </S.NaverButton>
