7b1c7494391ba6afbd947d3a9a37e7fa503e38fe
DefineXX <keaikim21@naver.com>
2024-12-29 15:56:31 +0900

    feat(auth): Access token 만료 시 axios response Interceptor로 토큰 재발급 요청 API 구현 (#42)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -11,12 +11,47 @@ axiosInstance.interceptors.request.use(
 
     // 토큰이 존재하면 토큰을 Authorization 헤더에 추가
     if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
     }
     return config;
   },
   (error) => {
-    return Promise.reject(error); // 요청 중 오류 발생 시 거부
+    return Promise.reject(error); // 에러 발생 시 상위 코드에서 에러 처리
+  },
+);
+
+// Response Interceptors
+axiosInstance.interceptors.response.use(
+  (response) => response, //  axios 요청 성공 시
+  async (error) => {
+    // axios 요청에 사용된 config object (URL, headers, body 포함)
+    const originalRequest = error.config;
+
+    // !originalRequest._retry는 요청 실패 시, 최초 한번만 재시도하도록 제어하는 역할
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        // Access Token 재발급 요청
+        const { data } = await axiosInstance.post('/auth/token_reissue');
+
+        const accessToken = data.access_token;
+        console.log('New Access Token', accessToken);
+
+        if (accessToken) {
+          localStorage.setItem('accessToken', accessToken);
+
+          originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+          return axiosInstance(originalRequest);
+        }
+      } catch {
+        // Access Token 재발급 실패 시
+        localStorage.removeItem('accessToken');
+        window.location.href = '/login';
+        console.error('Access Token reissue failed: ', error);
+      }
+    }
+    return Promise.reject(error);
   },
 );
 
